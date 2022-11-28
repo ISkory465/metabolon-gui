a6c6d923824839e66185c31cea826f5b5dc329a8
Iurii Skorokhod <skorokhod.iurii@gmail.com>
2022-11-28 10:10:39 +0100

    fix: main file renamed and moved to tree above

---
+++ metabolon.py
@@ -0,0 +1,52 @@
+import sys
+from PyQt5.QtWidgets import *
+import app_tabs.Strasse_1 as Strasse1
+import app_tabs.Steuerung_Strasse_2 as St_Strasse_2
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Metabolon Station")
+        self.setGeometry(350,150,900,600)
+        self.Tabs_UI()
+
+    def Tabs_UI(self):
+        mainLayout = QVBoxLayout()
+        self.tabs = QTabWidget()
+
+        #first Tab
+        self.tab1 = QWidget()
+        self.tabs.addTab(self.tab1,"Strasse 1")
+        Strasse1.UI(self)
+
+        #Second Tab
+        self.tab2 = QWidget()
+        # self.tabs.addTab(self.tab2,"Strasse 2")
+        # self.Tab_Strasse2()
+
+        #Third Tab
+        self.tab3 = QWidget()
+        # self.tabs.addTab(self.tab3,"Steuerung Strasse 1")
+        # self.Tab_Steuerung_Strasse1()
+
+        #Fourth Tab
+        self.tab4 = QWidget()
+        self.tabs.addTab(self.tab4,"Steuerung Strasse 2")
+        St_Strasse_2.UI(self)
+        
+                
+        mainLayout.addWidget(self.tabs)
+        self.setLayout(mainLayout)
+
+
+
+
+def main():
+    App=QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(App.exec_())
+
+if __name__=='__main__':
+    main()
