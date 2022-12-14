e76dc1b79853580af08c26a435207cf1e53581d9
Marina Garaeva <garaeva_marina@inbox.ru>
2022-12-14 12:54:44 +0100

    Modified Betriebsstunden,Stoermeldungen_Strasse_2 tabs and test_elements

--- components/app_tabs/Betriebsstunden.py
+++ components/app_tabs/Betriebsstunden.py
@@ -1,24 +1,69 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
-
+from ..test_element import InfoField
+from ..test_element import InfoFieldDouble
 
 
 def UI(self):
     #Main layout of the first tab 'Betriebsstunden"
-    vbox=QVBoxLayout()
-    
+    vbox1_1=QVBoxLayout()
+    vbox1_2=QVBoxLayout()
 
-    #test element:
-    hbox=QHBoxLayout()
-    label = QLabel('Marina Betriebsstunden')
-    
-    hbox.addWidget(label)
-    vbox.addLayout(hbox)
-    vbox.setAlignment(Qt.AlignTop)
-    
+    vbox2_1=QVBoxLayout()
+    vbox2_2=QVBoxLayout()
+    vbox2_3=QVBoxLayout()
+
+    hbox1=QHBoxLayout()
+    hbox2=QHBoxLayout()
+
+    grid = QGridLayout()
+
+
+    field1_1 = InfoFieldDouble(name = "HE11_BH",
+                         layout = vbox1_1, dec_num = 2)
+    field1_2 = InfoFieldDouble(name = "HE12_BH",
+                         layout = vbox1_1, dec_num = 0)
+    field1_3 = InfoFieldDouble(name = "HE21_BH",
+                         layout = vbox1_2, dec_num = 0)
+    field1_4 = InfoFieldDouble(name = "HE22_BH",
+                         layout = vbox1_2, dec_num = 0)   
+
+
+    field2_1 = InfoFieldDouble(name = "PU11_BH",
+                         layout = vbox2_1, dec_num = 3)
+    field2_2 = InfoFieldDouble(name = "PU12_BH",
+                         layout = vbox2_1, dec_num = 4)
+    field2_3 = InfoFieldDouble(name = "PU21_BH",
+                         layout = vbox2_2)
+    field2_4 = InfoFieldDouble(name = "PU22_BH",
+                         layout = vbox2_2, dec_num = 4)       
+    field2_5 = InfoFieldDouble(name = "PU31_BH",
+                         layout = vbox2_3, dec_num = 4)   
+
+    vbox2_3.setAlignment(Qt.AlignTop)
+
+    hbox1.addLayout(vbox1_1)
+    hbox1.addLayout(vbox1_2)
+
+    hbox2.addLayout(vbox2_1)
+    hbox2.addLayout(vbox2_2)
+    hbox2.addLayout(vbox2_3)
+
+    hbox1.setAlignment(Qt.AlignTop)
+    hbox1.setSpacing(5)
+
+    hbox2.setAlignment(Qt.AlignTop)
+    hbox2.setSpacing(5)
+
+    grid.setAlignment(Qt.AlignCenter)
+    grid.setSpacing(50)
+
+    grid.addLayout(hbox1, *[1,0])
+    grid.addLayout(hbox2, *[1,1])
+  
 
     #Assigning to the tab
-    self.tab7.setLayout(vbox)
+    self.tab7.setLayout(grid)
 
 
 if __name__=='__main__':
--- components/app_tabs/Stoermeldungen_Strasse_2.py
+++ components/app_tabs/Stoermeldungen_Strasse_2.py
@@ -1,24 +1,90 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
+from QLed import QLed
+
+from ..test_element import Box
+from ..test_element import InfoField
+from ..test_element import SingleLed
+from ..test_element import Led_6
+from ..test_element import Led_8
 
 
 
 def UI(self):
     #Main layout of the first tab 'Störmeldungen Straße 2"
-    vbox=QVBoxLayout()
-    
+    grid = QGridLayout()
+    vbox1=QVBoxLayout()
+    vbox2=QVBoxLayout()
 
     #test element:
     hbox=QHBoxLayout()
-    label = QLabel('Marina')
+
+    board_layout = QFormLayout()
     
-    hbox.addWidget(label)
-    vbox.addLayout(hbox)
-    vbox.setAlignment(Qt.AlignTop)
+   
+    #label = QLabel('Marina')
 
+    hbox.setAlignment(Qt.AlignCenter)
+    # vbox1.setAlignment(Qt.AlignCenter)
+    # vbox2.setAlignment(Qt.AlignCenter)
+    #hbox.addWidget(label)
+
+    #Second column elements of vbox6 for the hbox2:
+
+    led_l = Led_6(name="PU21 Antriebsstörung +FE02\
+                        ,PU21 Trockenlaufstörung +FE02\
+                        ,PU21 MaxDruckstörung +FE02\
+                        ,PU22 Antriebsstörung +FE02\
+                        ,PU22 Trockenlaufstörung +FE02\
+                        ,PU22 MaxDruckstörung +FE02", layout=vbox1)
+    led_r = Led_8(name="RW21 Antriebsstörung +FE03\
+                        ,RW21 FU-Störung +FE03\
+                        ,RW22 Antriebsstörung +FE03\
+                        ,RW22 FU-Störung +FE03\
+                        ,RW23 Antriebsstörung +FE03\
+                        ,RW23 FU-Störung +FE03\
+                        ,RW24 Antriebsstörung +FE03\
+                        ,RW24 FU-Störung +FE03", layout=vbox2)
+
+    
+
+    #hbox.addLayout(vbox1,2)
+    #hbox.addLayout(vbox2,1)
+
+    
+    #vbox1.add
+
+    #board_layout.addRow(vbox1)
+    #board_layout.addRow(vbox2)
+    #grid.setContentsMargins(10,10,20,20)
+    grid.addLayout(vbox1, *[0,0])
+    grid.addLayout(vbox2, *[0,1])
+
+   #grid.addWidget(led1)
+    #grid.addWidget(led2)
+
+
+    #self.sliderframe = QFrame(self.tab6)
+    #self.sliderframe.setFixedHeight(450)
+    #self.sliderframe.setFixedWidth(90)
+    #self.sliderframe.setStyleSheet("border: 1px solid grey; background: 0%;")
+    #self.sliderframe.setGeometry(200, 100, 450, 200)
+
+    #grid.addLayout(hbox, *[0,0])
+    
+    
+    #grid.setSpacing(10)
+    grid.setAlignment(Qt.AlignCenter)
+    grid.setHorizontalSpacing(30)
+    grid.setSpacing(20)
+    grid.setVerticalSpacing(60)
+    
     #Assigning to the tab
-    self.tab6.setLayout(vbox)
+    self.tab6.setLayout(grid)
+    #self.addWidget(self.tab6)
 
+ 
+      
 
 if __name__=='__main__':
     UI()
