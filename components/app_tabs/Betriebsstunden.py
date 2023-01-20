a3059c29d2c68964c5e3dd59b1c0efd292868437
Iurii Skorokhod <skorokhod.iurii@gmail.com>
2023-01-20 16:55:34 +0100

    Class created; variables assigned under the class and adeed to the objectList

--- components/app_tabs/Betriebsstunden.py
+++ components/app_tabs/Betriebsstunden.py
@@ -3,68 +3,94 @@ from PyQt5.QtCore import Qt
 from ..test_element import InfoField
 from ..test_element import InfoFieldDouble
 
+class Page():
 
-def UI(self):
-    #Main layout of the first tab 'Betriebsstunden"
-    vbox1_1=QVBoxLayout()
-    vbox1_2=QVBoxLayout()
+    def UI(self, window:QMainWindow):
+        
+        #Main layout of the first tab 'Betriebsstunden"
+        vbox1_1=QVBoxLayout()
+        vbox1_2=QVBoxLayout()
 
-    vbox2_1=QVBoxLayout()
-    vbox2_2=QVBoxLayout()
-    vbox2_3=QVBoxLayout()
+        vbox2_1=QVBoxLayout()
+        vbox2_2=QVBoxLayout()
+        vbox2_3=QVBoxLayout()
 
-    hbox1=QHBoxLayout()
-    hbox2=QHBoxLayout()
+        hbox1=QHBoxLayout()
+        hbox2=QHBoxLayout()
 
-    grid = QGridLayout()
-
-
-    field1_1 = InfoFieldDouble(name = "HE11_BH",
-                         layout = vbox1_1, dec_num = 2)
-    field1_2 = InfoFieldDouble(name = "HE12_BH",
-                         layout = vbox1_1, dec_num = 0)
-    field1_3 = InfoFieldDouble(name = "HE21_BH",
-                         layout = vbox1_2, dec_num = 0)
-    field1_4 = InfoFieldDouble(name = "HE22_BH",
-                         layout = vbox1_2, dec_num = 0)   
+        grid = QGridLayout()
 
 
-    field2_1 = InfoFieldDouble(name = "PU11_BH",
-                         layout = vbox2_1, dec_num = 3)
-    field2_2 = InfoFieldDouble(name = "PU12_BH",
-                         layout = vbox2_1, dec_num = 4)
-    field2_3 = InfoFieldDouble(name = "PU21_BH",
-                         layout = vbox2_2)
-    field2_4 = InfoFieldDouble(name = "PU22_BH",
-                         layout = vbox2_2, dec_num = 4)       
-    field2_5 = InfoFieldDouble(name = "PU31_BH",
-                         layout = vbox2_3, dec_num = 4)   
+        self.field1_1 = InfoFieldDouble(name = "HE11_BH",
+                            layout = vbox1_1, dec_num = 2)
+        self.field1_2 = InfoFieldDouble(name = "HE12_BH",
+                            layout = vbox1_1, dec_num = 0)
+        self.field1_3 = InfoFieldDouble(name = "HE21_BH",
+                            layout = vbox1_2, dec_num = 0)
+        self.field1_4 = InfoFieldDouble(name = "HE22_BH",
+                            layout = vbox1_2, dec_num = 0)   
 
-    vbox2_3.setAlignment(Qt.AlignTop)
 
-    hbox1.addLayout(vbox1_1)
-    hbox1.addLayout(vbox1_2)
+        self.field2_1 = InfoFieldDouble(name = "PU11_BH",
+                            layout = vbox2_1, dec_num = 3)
+        self.field2_2 = InfoFieldDouble(name = "PU12_BH",
+                            layout = vbox2_1, dec_num = 4)
+        self.field2_3 = InfoFieldDouble(name = "PU21_BH",
+                            layout = vbox2_2)
+        self.field2_4 = InfoFieldDouble(name = "PU22_BH",
+                            layout = vbox2_2, dec_num = 4)       
+        self.field2_5 = InfoFieldDouble(name = "PU31_BH",
+                            layout = vbox2_3, dec_num = 4)   
 
-    hbox2.addLayout(vbox2_1)
-    hbox2.addLayout(vbox2_2)
-    hbox2.addLayout(vbox2_3)
+        vbox2_3.setAlignment(Qt.AlignTop)
 
-    hbox1.setAlignment(Qt.AlignTop)
-    hbox1.setSpacing(5)
+        hbox1.addLayout(vbox1_1)
+        hbox1.addLayout(vbox1_2)
 
-    hbox2.setAlignment(Qt.AlignTop)
-    hbox2.setSpacing(5)
+        hbox2.addLayout(vbox2_1)
+        hbox2.addLayout(vbox2_2)
+        hbox2.addLayout(vbox2_3)
 
-    grid.setAlignment(Qt.AlignCenter)
-    grid.setSpacing(50)
+        hbox1.setAlignment(Qt.AlignTop)
+        hbox1.setSpacing(5)
 
-    grid.addLayout(hbox1, *[1,0])
-    grid.addLayout(hbox2, *[1,1])
-  
+        hbox2.setAlignment(Qt.AlignTop)
+        hbox2.setSpacing(5)
 
-    #Assigning to the tab
-    self.tab7.setLayout(grid)
+        grid.setAlignment(Qt.AlignCenter)
+        grid.setSpacing(50)
 
+        grid.addLayout(hbox1, *[1,0])
+        grid.addLayout(hbox2, *[1,1])
+    
+
+        #Assigning to the tab
+        window.tab7.setLayout(grid)
+
+
+
+    def updateAll(self,inputs: dict):
+        """method to update all objects in current tab periodically after reading the values in different thread
+
+        :param inputs: tag values
+        :type inputs: dict
+        """
+        objectList=[self.field1_1,
+                    self.field1_2,
+                    self.field1_3,
+                    self.field1_4,
+
+                    self.field2_1,
+                    self.field2_2,
+                    self.field2_3,
+                    self.field2_4,
+                    self.field2_5]
+
+
+        for o in objectList:
+            #iterate over an update method that should be added to all faceplate objects similar to box object
+            o.update(inputs)
+        
 
 if __name__=='__main__':
-    UI()
+    pass
