0f51d777b41deb5a93e63f7338108c410843319e
Vladyslav Borysenko <borys.vladyslav@gmail.com>
2023-05-29 10:20:33 +0200

    Mixer widget with M button

--- deprecated/test circle 7.py
+++ deprecated/test circle 7.py
@@ -1,19 +1,20 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
 from PyQt5.QtGui import QPainter, QColor, QPen, QFont, QFontMetrics
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QRectF, QPoint, QRect
 
-
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
 
 class BigMixer(QWidget):
     def __init__(self, level=10):
         super().__init__()
-        self.setMinimumSize(100, 200)  # Increased the minimum width to accommodate the ticks and labels
+        self.setMinimumSize(400, 400)
         self.level = level
         self.state = 0  # Initial state for level
+
+        self.initUI()
+
+    def initUI(self):
+        layout = QVBoxLayout(self)  # Create a vertical layout for the widget
 
     def setLevel(self, val):
         self.level = val
@@ -26,43 +27,46 @@ class BigMixer(QWidget):
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.setRenderHint(QPainter.Antialiasing)
+        painter.setClipRect(self.rect(), Qt.ReplaceClip)
 
         # Draw the background
         painter.fillRect(self.rect(), Qt.lightGray)
 
-        # # Calculate the scale position based on the level value
-        # scale_width = int((self.width() - 30) * (self.level) / (100))
-        # scale_rect = QRect(10, self.height() - 10 - scale_width, self.width() - 70, scale_width)
-        # painter.fillRect(scale_rect, Qt.red)
-
         # Calculate the scale position based on the level value
-        scale_height = int((self.height()) * (self.level) / 100)
-        scale_rect = QRect(10, 10, self.width() - 20, self.height() - 30 - scale_height)
+        buffer = 100  # Adjust the buffer value to control the empty space at the top
+        scale_height = int((self.height() - buffer - 20) * self.level / 100)
+        scale_rect = QRect(10, self.height() - 10 - scale_height - 1, self.width() - 70, scale_height + 1)
         painter.fillRect(scale_rect, Qt.red)
-
 
         # Draw small rectangles in the top corners
         rectangle_size = 40
         rectangle_spacing = 10
-        rectangle1 = QRect(rectangle_spacing + 10, rectangle_spacing, rectangle_size + 20, rectangle_size)
-        rectangle2 = QRect(self.width() - rectangle_size - rectangle_spacing - 80, rectangle_spacing, rectangle_size + 20, rectangle_size)
+        rectangle1 = QRect(rectangle_spacing + 10, buffer + rectangle_spacing + 8, rectangle_size + 20, rectangle_size)
+        rectangle2 = QRect(self.width() - rectangle_size - rectangle_spacing - 80, buffer + rectangle_spacing + 8, rectangle_size + 20, rectangle_size)
 
         # Determine the color based on the state
         rectangle1_color = Qt.green if self.state == 1 else Qt.gray
         rectangle2_color = Qt.green if self.state == 2 else Qt.gray
 
-        painter.fillRect(rectangle1, rectangle1_color)
-        painter.fillRect(rectangle2, rectangle2_color)
+        # Draw the outline/frame
+        outline_pen = QPen(Qt.black, 1)
+        painter.setPen(outline_pen)
+        painter.drawRect(rectangle1)
+        painter.drawRect(rectangle2)
+
+        painter.fillRect(rectangle1.adjusted(1, 1, -1, -1), rectangle1_color)
+        painter.fillRect(rectangle2.adjusted(1, 1, -1, -1), rectangle2_color)
 
         # Draw ticks and labels on the right side
         font = QFont()
         font.setPointSize(12)
         painter.setFont(font)
         tick_length = 5
-        tick_spacing = (self.height() - 20) / (100)
+        tick_spacing = (self.height() - buffer - 20) / 100
         degree_spacing = 5
         for level in range(0, 101, degree_spacing):
-            y = self.height() - 10 - int((level) * tick_spacing)
+            y = self.height() - 10 - int(level * tick_spacing)
+            # y = buffer + self.height() - 30 - int((level / 100) * (self.height() - buffer))  # Adjusted y coordinate calculation
             if level in [0, 50, 100]:  # Display labels for values 0, 50, and 100 only
                 label_font = painter.font()
                 label_font.setBold(True)
@@ -82,6 +86,24 @@ class BigMixer(QWidget):
 
             painter.drawLine(self.width() - tick_length - 40, y, self.width() - 60, y)
 
+        # Draw the infinity sign
+        ellipse1 = QRectF(60, self.height() - 100, 100, 30)
+        ellipse2 = QRectF(140, self.height() - 100, 100, 30)
+        painter.drawEllipse(ellipse1)
+        painter.drawEllipse(ellipse2)
+
+        # Draw the perpendicular line
+        intersection_point = ellipse1.topRight().toPoint()
+        line_start = QPoint(150, self.height() - 85)
+        line_end = QPoint(150, buffer // 2)
+        painter.drawLine(line_start, line_end)
+
+        # Draw the circle where the M button should be
+        circle_radius = 30
+        circle_center = QPoint(150, buffer // 2)
+        circle_rect = QRectF(circle_center.x() - circle_radius, circle_center.y() - circle_radius, 2 * circle_radius, 2 * circle_radius)
+        painter.drawEllipse(circle_rect)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -90,9 +112,6 @@ if __name__ == '__main__':
 
     tank.setState(1)
     tank.setState(2)
-    tank.setLevel(20)
-
-    # Trigger a repaint of the tank
-    tank.update()
+    tank.setLevel(25)
 
     sys.exit(app.exec_())
