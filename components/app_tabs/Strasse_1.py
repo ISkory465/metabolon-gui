ded0b8e9600bc4d52d324a4b093398d2dba54598
Adilkhan <arsenal.rvp41@gmail.com>
2023-06-04 00:22:35 +0200

    feat: add faceplate_tankibc

--- components/app_tabs/Strasse_1.py
+++ components/app_tabs/Strasse_1.py
@@ -1,8 +1,10 @@
 from PyQt5.QtWidgets    import *
 from PyQt5.QtCore       import Qt
-from ..faceplates.facelpates_new   import Box
+from ..faceplates.facelpates_new import Box
+from ..faceplates.faceplates import Box
 from ..faceplates.faceplate_mixer  import Mixer
 from ..faceplates.faceplate_endlager import Endlager
+from ..faceplates.faceplate_tankibc import TankIBC
 
 
 
@@ -16,8 +18,9 @@ class Page():
 
         #Tab is split into three horizontal boxes
         self.hbox1 = QHBoxLayout()
-        self.hbox2 = QHBoxLayout()
-        self.hbox3 = QHBoxLayout()
+        #self.hbox2 = QHBoxLayout()
+        self.hbox2 = QGridLayout()
+        self.hbox3 = QGridLayout()
 
         #First horizontal hbox1 layout contains three vertical boxes
         self.vbox1_1 = QVBoxLayout()
@@ -43,7 +46,38 @@ class Page():
         self.mixer2 = Mixer(name="Nachgärer", layout = self.vbox1_2)
         self.endlager = Endlager(name="Endlager", layout = self.vbox1_3)
 
-
+        # hbox2 Content
+        #self.box1 = Box("PU12", self.hbox2)
+        self.tankIbc = TankIBC(max_level=100, min_level=15)
+        self.tankIbc.set_current_level(10)
+        self.tankIbc.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Preferred)
+        #self.hbox2.addWidget(self.tankIbc)
+        #self.hbox2.setAlignment(Qt.AlignRight)
+        
+         # Create the TankIBC widget
+        self.hbox2.addWidget(self.tankIbc.get_widget(), 0, 0)  # Add the tank widget to the grid layout
+        self.hbox2.setColumnStretch(0, 0)
+        
+        # Create and add the Box widget
+        self.box1 = Box("PU12")
+        self.hbox2.addWidget(self.box1, 0, 6)  # Add the box widget to the grid layout
+        
+        # hbox3 Content
+        self.box2 = Box("PU11")
+        self.box3 = Box("RW11")
+        self.box4 = Box("RW12")
+        self.box5 = Box("AA11")
+        self.box6 = Box("AA12")
+        self.box7 = Box("AA13")
+        self.box8 = Box("AA14")
+        
+        self.hbox3.addWidget(self.box2, 0, 0)
+        self.hbox3.addWidget(self.box3, 0, 1)
+        self.hbox3.addWidget(self.box4, 0, 3)
+        self.hbox3.addWidget(self.box5, 0, 4)
+        self.hbox3.addWidget(self.box6, 0, 5)
+        self.hbox3.addWidget(self.box7, 0, 6)
+        self.hbox3.addWidget(self.box8, 0, 7)
 
 
 if __name__=='__main__':
