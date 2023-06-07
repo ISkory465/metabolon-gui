ba92dd8012afdb8c902add8d8eb54fb75d984e33
Vladyslav Borysenko <borys.vladyslav@gmail.com>
2023-06-07 13:20:00 +0200

    Finished documenting side bar Adjusted the gauge to a new scale

--- components/faceplates/faceplate_gauge.py
+++ components/faceplates/faceplate_gauge.py
@@ -1,6 +1,6 @@
 import math
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtCore import Qt
 
 
@@ -20,9 +20,14 @@ class Gauge(QWidget):
         painter.setWindow(0, 0, 100, 100)
         painter.setPen(Qt.NoPen)
 
+        # Draw outline
+        painter.setBrush(Qt.black)
+        painter.drawEllipse(4, 4, 92, 92)
+
         # Draw background
         painter.setBrush(Qt.white)
         painter.drawEllipse(5, 5, 90, 90)
+
 
         # Draw ticks and labels
         painter.setPen(QPen(Qt.black, 2))
@@ -32,10 +37,10 @@ class Gauge(QWidget):
         metrics = painter.fontMetrics()
         label_height = metrics.height()
 
-        for i in range(6):
-            angle = 225 + i * 60
-            x1, y1 = map(int, self._get_point(angle, 0.85))
-            x2, y2 = map(int, self._get_point(angle, 0.95))
+        for i in range(-2, 7):
+            angle = 0 + i * 36
+            x1, y1 = map(int, self._get_point(angle, 0.85)) # closer to the center of the circle
+            x2, y2 = map(int, self._get_point(angle, 0.95)) # farther from the center of the circle
             x3, y3 = map(int, self._get_point(angle, 0.55))
             painter.drawLine(x1, y1, x2, y2)
 
@@ -43,17 +48,18 @@ class Gauge(QWidget):
             label_width = metrics.width(str(label_value))
             label_x = x3 - (label_width) // 2 
             label_y = y3 + (label_height-5) // 2 # Adjust the label position
-            painter.drawText(label_x, label_y, str(label_value))
+            if label_value % 10 == 0:
+                painter.drawText(label_x, label_y, str(label_value))
 
         # Draw needle
         painter.setPen(QPen(Qt.red, 2))
-        angle = 225 + self.value * 12
+        angle = 270 + self.value * 12
         x1, y1 = map(int, self._get_point(angle, 0.15))
         x2, y2 = map(int, self._get_point(angle, 0.85))
         painter.drawLine(x1, y1, x2, y2)
 
     def _get_point(self, angle, radius):
-        angle_rad = math.radians(495 - angle)
+        angle_rad = math.radians(540 - angle)
         x = 50 + math.cos(angle_rad) * 40 * radius
         y = 50 - math.sin(angle_rad) * 40 * radius
         return x, y
--- components/faceplates/faceplate_side_bar.py
+++ components/faceplates/faceplate_side_bar.py
@@ -10,7 +10,6 @@ from components.faceplates.faceplates_new import Led_5, SingleLed
 from components.faceplates.faceplate_email_button import EmailButton
 from components.faceplates.faceplate_stop_button import STOPButton
 
-# FIXME: Finish the documentation and make sure everything works
 
 class SideBarFaceplate(QGroupBox):
     """
@@ -22,25 +21,31 @@ class SideBarFaceplate(QGroupBox):
 
         # Main layout of the sidebar
         self.vbox = QVBoxLayout()
-        self.vbox.setAlignment(Qt.AlignTop)
+        self.vbox.setAlignment(Qt.AlignTop) # might not be necessary at this point
 
         # Instantiate components and add to the layout
+        # Controls (user input) lock button
         self.on_off_lock = OnOffButton()
         self.vbox.addWidget(self.on_off_lock)
 
+        # Fault reporting box
         self.fault_box = FaultBox()
         self.vbox.addWidget(self.fault_box)
 
+        # Fackel (torch) operation block
         self.fackel_box = FackelBox(name="Fackel", layout=self.vbox)
         
+        # Controls box
         self.stoerungBox = Led_5(box_name="Stoerungen", name="ML Sammelstoerung\
                             ,FE01 (Einspeisung / Steuerspannung)\
                             ,FE02 (Antriebe Pumpen / Diverse)\
                             ,FE03 (Antriebe Ruehrwerke)\
                             ,FE04 (SPS / MSR Technik)", layout=self.vbox)
         
+        # Pump status
         self.pumpeLED = SingleLed(name="Pumpe PU31 (Mobil)", layout=self.vbox)
 
+        # Email notifications button
         self.email = EmailButton()
         self.vbox.addWidget(self.email)
 
@@ -50,9 +55,10 @@ class SideBarFaceplate(QGroupBox):
         self.stop2 = STOPButton(name="Stop")
         stop_hbox.addWidget(self.stop1)
         stop_hbox.addWidget(self.stop2)
-        self.vbox.addLayout(stop_hbox)
+        self.vbox.addLayout(stop_hbox) # add two buttons to the main layout
 
         # Set layout in a container, add to a scroll area
+        # This is needed to ensure the whole side bar is not shifted in the full-screen mode
         container = QWidget()
         container.setLayout(self.vbox)
         scroll = QScrollArea()
