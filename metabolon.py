5751569afbcbd625f87f67cdb52545ea129c87d2
Iurii Skorokhod <skorokhod.iurii@gmail.com>
2023-06-09 10:22:55 +0200

    Updater: page instances created for the opc part

--- metabolon.py
+++ metabolon.py
@@ -62,20 +62,41 @@ class Window(QMainWindow):
         # Hide the tab bar of the outer QTabWidget so only the inner tab bars are visible
         self.tabs.tabBar().setVisible(True)
 
+
+        # <--self.innerTabs1-->
+        #Page instances for the import to the self.innerTabs1
+        self.strasse_1 = Strasse_1()
+        self.steu_strasse_1 = St_Strasse_1()
+        self.fuet_strasse_1 = Fuet_Strasse_1()
+        self.steu_meld_starsse_1 = St_Meld_1()
+
         # Tabs connection to the self.innerTabs1:
-        self.innerTabs1.addTab(Strasse_1(),       "Übersicht")       #old name: "Strasse 1"
-        self.innerTabs1.addTab(St_Strasse_1(),    "Steuerung")       #old name: "Steuerung Strasse 1"
-        self.innerTabs1.addTab(Fuet_Strasse_1(),  "Fütterung")       #old name: "Fütterung Straße 1"
-        self.innerTabs1.addTab(St_Meld_1(),       "Störmeldungen")   #old name: "Störmeldungen Straße 1"
+        self.innerTabs1.addTab(self.strasse_1,              "Übersicht")       #old name: "Strasse 1"
+        self.innerTabs1.addTab(self.steu_strasse_1,         "Steuerung")       #old name: "Steuerung Strasse 1"
+        self.innerTabs1.addTab(self.fuet_strasse_1,         "Fütterung")       #old name: "Fütterung Straße 1"
+        self.innerTabs1.addTab(self.steu_meld_starsse_1,    "Störmeldungen")   #old name: "Störmeldungen Straße 1"
+
+
+        # <--self.innerTabs2-->
+        #Page instances for the import to the self.innerTabs2
+        self.strasse_2 = Strasse_2()
+        self.steu_strasse_2 = St_Strasse_2()
+        self.fuet_strasse_2 = Fuet_Strasse_2()
+        self.steu_meld_starsse_2 = St_Meld_2()
 
         # Tabs connection to the self.innerTabs2:      
-        self.innerTabs2.addTab(Strasse_2(),       "Übersicht")      #old name: "Strasse 2"
-        self.innerTabs2.addTab(St_Strasse_2(),    "Steuerung")      #old name: "Steuerung Strasse 2"
-        self.innerTabs2.addTab(Fuet_Strasse_2(),  "Fütterung")      #old name: "Fütterung Straße 2"
-        self.innerTabs2.addTab(St_Meld_2(),       "Störmeldungen")  #old name: "Störmeldungen Straße 2"
+        self.innerTabs2.addTab(self.strasse_2,              "Übersicht")        #old name: "Strasse 2"
+        self.innerTabs2.addTab(self.steu_strasse_2,         "Steuerung")        #old name: "Steuerung Strasse 2"
+        self.innerTabs2.addTab(self.fuet_strasse_2,         "Fütterung")        #old name: "Fütterung Straße 2"
+        self.innerTabs2.addTab(self.steu_meld_starsse_2,    "Störmeldungen")    #old name: "Störmeldungen Straße 2"
+
+
+        # <--self.innerTabs3-->
+        #Page instances for the import to the self.innerTabs3
+        self.betrieb = Betriebsstunden()
 
         # Tab connection to the self.innerTabs3:
-        self.innerTabs3.addTab(Betriebsstunden(), "Betriebsstunden")
+        self.innerTabs3.addTab(self.betrieb, "Betriebsstunden")
 
 
         # Create a central widget to hold the mainLayout
